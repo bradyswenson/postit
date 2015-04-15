b9e0010fb0a4bb1062bb64aa98ab3590419c7448
Brady Swenson <bradyswenson@gmail.com>
2015-04-15 15:51:08 -0500

    update to voting

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :voteable, polymorphic: true
 
-  validates_uniqueness_of :creator, scope: :voteable
+  validates_uniqueness_of :creator, scope: [:voteable_id, :voteable_type]
 end
